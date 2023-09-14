1796f5a9af976f0515bfe71931cb669a2b0bfe34
Paul Lange <palango@gmx.de>
2023-09-14 14:16:23 +0200

    Add celo transaction type

--- core/types/transaction.go
+++ core/types/transaction.go
@@ -49,6 +49,8 @@ const (
 	AccessListTxType = 0x01
 	DynamicFeeTxType = 0x02
 	BlobTxType       = 0x03
+	// CeloDynamicFeeTxType = 0x7c  old Celo tx type with gateway fee
+	CeloDynamicFeeTxType = 0x7b
 )
 
 // Transaction is an Ethereum transaction.
@@ -110,6 +112,9 @@ type TxData interface {
 
 	encode(*bytes.Buffer) error
 	decode([]byte) error
+
+	// Celo specific fields
+	feeCurrency() *common.Address
 }
 
 // EncodeRLP implements rlp.Encoder
@@ -214,6 +219,8 @@ func (tx *Transaction) decodeTyped(b []byte) (TxData, error) {
 		inner = new(AccessListTx)
 	case DynamicFeeTxType:
 		inner = new(DynamicFeeTx)
+	case CeloDynamicFeeTxType:
+		inner = new(CeloDynamicFeeTx)
 	case BlobTxType:
 		inner = new(BlobTx)
 	case DepositTxType:
@@ -656,6 +663,11 @@ func (tx *Transaction) WithSignature(signer Signer, sig []byte) (*Transaction, e
 	cpy := tx.inner.copy()
 	cpy.setSignatureValues(signer.ChainID(), v, r, s)
 	return &Transaction{inner: cpy, time: tx.time}, nil
+}
+
+// FeeCurrency returns the fee currency of the transaction. Nil implies paying in CELO.
+func (tx *Transaction) FeeCurrency() *common.Address {
+	return copyAddressPtr(tx.inner.feeCurrency())
 }
 
 // Transactions implements DerivableList for transactions.
