celo-org/op-geth

Read IntrinsicGas from FeeCurrencyDirectory  (#178)
* Add FeeCurrencyContext object to BlockContext

This replaces the `BlockContext.ExchangeRates` with a wrapper object
`BlockContext.FeeCurrencyContext`.

This enables easier addition of fee-currency related information
to the block context and passing around that information.
The exchange-rates are now part of the `FeeCurrencyContext`

* Add intrinsic gas retrieval from FeeCurrencyDirectory

* Use intrinsic gas from FeeCurrencyDirectory in STF

* Add test for intrinsic gas too high in fee currency

* Add empty fee-currency context to tools and tests

* Add minor code improvements

Co-authored-by: Paul Lange <palango@users.noreply.github.com>

* Add comment for nil fee-currency code path

* Add fee-currency-context to CeloBackend.NewEVM constructor

---------

Co-authored-by: Paul Lange <palango@users.noreply.github.com>

core/txpool/celo_validation.go
@@ -1,15 +1,13 @@
 package txpool
 
 import (
-	"errors"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/exchange"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/params"
 )
-
-var NonWhitelistedFeeCurrencyError = errors.New("Fee currency given is not whitelisted at current block")
 
 // AcceptSet is a set of accepted transaction types for a transaction subpool.
 type AcceptSet = map[uint8]struct{}
@@ -50,12 +48,12 @@ func (cvo *CeloValidationOptions) Accepts(txType uint8) bool {
 // This check is public to allow different transaction pools to check the basic
 // rules without duplicating code and running the risk of missed updates.
 func CeloValidateTransaction(tx *types.Transaction, head *types.Header,
-	signer types.Signer, opts *CeloValidationOptions, rates common.ExchangeRates) error {
-	if err := ValidateTransaction(tx, head, signer, opts); err != nil {
+	signer types.Signer, opts *CeloValidationOptions, currencyCtx common.FeeCurrencyContext) error {
+	if err := ValidateTransaction(tx, head, signer, opts, currencyCtx); err != nil {
 		return err
 	}
-	if !common.IsCurrencyWhitelisted(rates, tx.FeeCurrency()) {
-		return NonWhitelistedFeeCurrencyError
+	if !common.IsCurrencyWhitelisted(currencyCtx.ExchangeRates, tx.FeeCurrency()) {
+		return exchange.ErrNonWhitelistedFeeCurrency
 	}
 
 	return nil

core/txpool/legacypool/celo_legacypool.go
@@ -12,7 +12,7 @@ import (
 // and gasLimit. Returns drops and invalid txs.
 func (pool *LegacyPool) filter(list *list, addr common.Address, gasLimit uint64) (types.Transactions, types.Transactions) {
 	// CELO: drop all transactions that no longer have a whitelisted currency
-	dropsWhitelist, invalidsWhitelist := list.FilterWhitelisted(pool.currentRates)
+	dropsWhitelist, invalidsWhitelist := list.FilterWhitelisted(pool.feeCurrencyContext.ExchangeRates)
 	// Check from which currencies we need to get balances
 	currenciesInList := list.FeeCurrencies()
 	drops, invalids := list.Filter(pool.getBalances(addr, currenciesInList), gasLimit)
@@ -34,9 +34,10 @@ func (pool *LegacyPool) recreateCeloProperties() {
 		ChainConfig: pool.chainconfig,
 		State:       pool.currentState,
 	}
-	currentRates, err := contracts.GetExchangeRates(pool.celoBackend)
+	feeCurrencyContext, err := contracts.GetFeeCurrencyContext(pool.celoBackend)
 	if err != nil {
-		log.Error("Error trying to get exchange rates in txpool.", "cause", err)
+		log.Error("Error trying to get fee currency context in txpool.", "cause", err)
 	}
-	pool.currentRates = currentRates
+
+	pool.feeCurrencyContext = feeCurrencyContext
 }

Tenderly/net-celo

core/txpool/celo_validation.go
@@ -1,7 +1,6 @@
 package txpool
 
 import (
-	"errors"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -9,8 +8,6 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/params"
 )
-
-var NonWhitelistedFeeCurrencyError = errors.New("Fee currency given is not whitelisted at current block")
 
 // AcceptSet is a set of accepted transaction types for a transaction subpool.
 type AcceptSet = map[uint8]struct{}
@@ -51,11 +48,11 @@ func (cvo *CeloValidationOptions) Accepts(txType uint8) bool {
 // This check is public to allow different transaction pools to check the basic
 // rules without duplicating code and running the risk of missed updates.
 func CeloValidateTransaction(tx *types.Transaction, head *types.Header,
-	signer types.Signer, opts *CeloValidationOptions, rates common.ExchangeRates) error {
-	if err := ValidateTransaction(tx, head, signer, opts); err != nil {
+	signer types.Signer, opts *CeloValidationOptions, currencyCtx common.FeeCurrencyContext) error {
+	if err := ValidateTransaction(tx, head, signer, opts, currencyCtx); err != nil {
 		return err
 	}
-	if !common.IsCurrencyAllowed(rates, tx.FeeCurrency()) {
+	if !common.IsCurrencyAllowed(currencyCtx.ExchangeRates, tx.FeeCurrency()) {
 		return exchange.ErrUnregisteredFeeCurrency
 	}
 

core/txpool/legacypool/celo_legacypool.go
@@ -12,7 +12,7 @@ import (
 // and gasLimit. Returns drops and invalid txs.
 func (pool *LegacyPool) filter(list *list, addr common.Address, gasLimit uint64) (types.Transactions, types.Transactions) {
 	// CELO: drop all transactions that no longer have a registered currency
-	dropsAllowlist, invalidsAllowlist := list.FilterAllowlisted(pool.currentRates)
+	dropsAllowlist, invalidsAllowlist := list.FilterAllowlisted(pool.feeCurrencyContext.ExchangeRates)
 	// Check from which currencies we need to get balances
 	currenciesInList := list.FeeCurrencies()
 	drops, invalids := list.Filter(pool.getBalances(addr, currenciesInList), gasLimit)
@@ -34,9 +34,10 @@ func (pool *LegacyPool) recreateCeloProperties() {
 		ChainConfig: pool.chainconfig,
 		State:       pool.currentState,
 	}
-	currentRates, err := contracts.GetExchangeRates(pool.celoBackend)
+	feeCurrencyContext, err := contracts.GetFeeCurrencyContext(pool.celoBackend)
 	if err != nil {
-		log.Error("Error trying to get exchange rates in txpool.", "cause", err)
+		log.Error("Error trying to get fee currency context in txpool.", "cause", err)
 	}
-	pool.currentRates = currentRates
+
+	pool.feeCurrencyContext = feeCurrencyContext
 }