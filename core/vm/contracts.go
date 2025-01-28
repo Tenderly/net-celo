celo-org/op-geth

Add CeloPrecompiledContext

core/vm/contracts.go
@@ -124,6 +124,13 @@ var PrecompiledContractsFjord = map[common.Address]PrecompiledContract{
 	common.BytesToAddress([]byte{0x01, 0x00}): &p256Verify{},
 }
 
+// PrecompiledContractsCel2 contains the default set of pre-compiled Ethereum
+// contracts used in the Cel2 release which don't require the extra
+// celoPrecompileContext, while PrecompiledCeloContractsCel2 contains those
+// that do.
+var PrecompiledContractsCel2 = PrecompiledContractsCancun
+var PrecompiledCeloContractsCel2 = map[common.Address]CeloPrecompiledContract{}
+
 // PrecompiledContractsBLS contains the set of pre-compiled Ethereum
 // contracts specified in EIP-2537. These are exported for testing purposes.
 var PrecompiledContractsBLS = map[common.Address]PrecompiledContract{
@@ -139,6 +146,7 @@ var PrecompiledContractsBLS = map[common.Address]PrecompiledContract{
 }
 
 var (
+	PrecompiledAddressesCel2      []common.Address
 	PrecompiledAddressesFjord     []common.Address
 	PrecompiledAddressesCancun    []common.Address
 	PrecompiledAddressesBerlin    []common.Address
@@ -168,8 +176,9 @@ func init() {
 	}
 }
 
-// ActivePrecompiles returns the precompiles enabled with the current configuration.
-func ActivePrecompiles(rules params.Rules) []common.Address {
+// OptimismPrecompiles returns the original Optimism precompiles enabled with
+// the current configuration.
+func OptimismPrecompiles(rules params.Rules) []common.Address {
 	switch {
 	case rules.IsOptimismFjord:
 		return PrecompiledAddressesFjord
@@ -186,18 +195,36 @@ func ActivePrecompiles(rules params.Rules) []common.Address {
 	}
 }
 
+// ActivePrecompiles returns the precompiles enabled with the current configuration.
+func ActivePrecompiles(rules params.Rules) []common.Address {
+	addresses := OptimismPrecompiles(rules)
+
+	if !rules.IsCel2 {
+		return addresses
+	}
+
+	PrecompiledAddressesCel2 = PrecompiledAddressesCel2[:0]
+	PrecompiledAddressesCel2 = append(PrecompiledAddressesCel2, addresses...)
+
+	for k := range PrecompiledCeloContractsCel2 {
+		PrecompiledAddressesCel2 = append(PrecompiledAddressesCel2, k)
+	}
+
+	return PrecompiledAddressesCel2
+}
+
 // RunPrecompiledContract runs and evaluates the output of a precompiled contract.
 // It returns
 // - the returned bytes,
 // - the _remaining_ gas,
 // - any error that occurred
-func RunPrecompiledContract(p PrecompiledContract, input []byte, suppliedGas uint64) (ret []byte, remainingGas uint64, err error) {
+func RunPrecompiledContract(p CeloPrecompiledContract, input []byte, suppliedGas uint64, ctx *celoPrecompileContext) (ret []byte, remainingGas uint64, err error) {
 	gasCost := p.RequiredGas(input)
 	if suppliedGas < gasCost {
 		return nil, 0, ErrOutOfGas
 	}
 	suppliedGas -= gasCost
-	output, err := p.Run(input)
+	output, err := p.Run(input, ctx)
 	return output, suppliedGas, err
 }
 

core/vm/contracts_test.go
@@ -45,30 +45,30 @@ type precompiledFailureTest struct {
 
 // allPrecompiles does not map to the actual set of precompiles, as it also contains
 // repriced versions of precompiles at certain slots
-var allPrecompiles = map[common.Address]PrecompiledContract{
-	common.BytesToAddress([]byte{1}):    &ecrecover{},
-	common.BytesToAddress([]byte{2}):    &sha256hash{},
-	common.BytesToAddress([]byte{3}):    &ripemd160hash{},
-	common.BytesToAddress([]byte{4}):    &dataCopy{},
-	common.BytesToAddress([]byte{5}):    &bigModExp{eip2565: false},
-	common.BytesToAddress([]byte{0xf5}): &bigModExp{eip2565: true},
-	common.BytesToAddress([]byte{6}):    &bn256AddIstanbul{},
-	common.BytesToAddress([]byte{7}):    &bn256ScalarMulIstanbul{},
-	common.BytesToAddress([]byte{8}):    &bn256PairingIstanbul{},
-	common.BytesToAddress([]byte{9}):    &blake2F{},
-	common.BytesToAddress([]byte{0x0a}): &kzgPointEvaluation{},
+var allPrecompiles = map[common.Address]CeloPrecompiledContract{
+	common.BytesToAddress([]byte{1}):    &wrap{&ecrecover{}},
+	common.BytesToAddress([]byte{2}):    &wrap{&sha256hash{}},
+	common.BytesToAddress([]byte{3}):    &wrap{&ripemd160hash{}},
+	common.BytesToAddress([]byte{4}):    &wrap{&dataCopy{}},
+	common.BytesToAddress([]byte{5}):    &wrap{&bigModExp{eip2565: false}},
+	common.BytesToAddress([]byte{0xf5}): &wrap{&bigModExp{eip2565: true}},
+	common.BytesToAddress([]byte{6}):    &wrap{&bn256AddIstanbul{}},
+	common.BytesToAddress([]byte{7}):    &wrap{&bn256ScalarMulIstanbul{}},
+	common.BytesToAddress([]byte{8}):    &wrap{&bn256PairingIstanbul{}},
+	common.BytesToAddress([]byte{9}):    &wrap{&blake2F{}},
+	common.BytesToAddress([]byte{0x0a}): &wrap{&kzgPointEvaluation{}},
 
-	common.BytesToAddress([]byte{0x01, 0x00}): &p256Verify{},
+	common.BytesToAddress([]byte{0x01, 0x00}): &wrap{&p256Verify{}},
 
-	common.BytesToAddress([]byte{0x0f, 0x0a}): &bls12381G1Add{},
-	common.BytesToAddress([]byte{0x0f, 0x0b}): &bls12381G1Mul{},
-	common.BytesToAddress([]byte{0x0f, 0x0c}): &bls12381G1MultiExp{},
-	common.BytesToAddress([]byte{0x0f, 0x0d}): &bls12381G2Add{},
-	common.BytesToAddress([]byte{0x0f, 0x0e}): &bls12381G2Mul{},
-	common.BytesToAddress([]byte{0x0f, 0x0f}): &bls12381G2MultiExp{},
-	common.BytesToAddress([]byte{0x0f, 0x10}): &bls12381Pairing{},
-	common.BytesToAddress([]byte{0x0f, 0x11}): &bls12381MapG1{},
-	common.BytesToAddress([]byte{0x0f, 0x12}): &bls12381MapG2{},
+	common.BytesToAddress([]byte{0x0f, 0x0a}): &wrap{&bls12381G1Add{}},
+	common.BytesToAddress([]byte{0x0f, 0x0b}): &wrap{&bls12381G1Mul{}},
+	common.BytesToAddress([]byte{0x0f, 0x0c}): &wrap{&bls12381G1MultiExp{}},
+	common.BytesToAddress([]byte{0x0f, 0x0d}): &wrap{&bls12381G2Add{}},
+	common.BytesToAddress([]byte{0x0f, 0x0e}): &wrap{&bls12381G2Mul{}},
+	common.BytesToAddress([]byte{0x0f, 0x0f}): &wrap{&bls12381G2MultiExp{}},
+	common.BytesToAddress([]byte{0x0f, 0x10}): &wrap{&bls12381Pairing{}},
+	common.BytesToAddress([]byte{0x0f, 0x11}): &wrap{&bls12381MapG1{}},
+	common.BytesToAddress([]byte{0x0f, 0x12}): &wrap{&bls12381MapG2{}},
 }
 
 // EIP-152 test vectors
@@ -100,7 +100,7 @@ func testPrecompiled(addr string, test precompiledTest, t *testing.T) {
 	in := common.Hex2Bytes(test.Input)
 	gas := p.RequiredGas(in)
 	t.Run(fmt.Sprintf("%s-Gas=%d", test.Name, gas), func(t *testing.T) {
-		if res, _, err := RunPrecompiledContract(p, in, gas); err != nil {
+		if res, _, err := RunPrecompiledContract(p, in, gas, mockPrecompileContext); err != nil {
 			t.Error(err)
 		} else if common.Bytes2Hex(res) != test.Expected {
 			t.Errorf("Expected %v, got %v", test.Expected, common.Bytes2Hex(res))
@@ -122,7 +122,7 @@ func testPrecompiledOOG(addr string, test precompiledTest, t *testing.T) {
 	gas := p.RequiredGas(in) - 1
 
 	t.Run(fmt.Sprintf("%s-Gas=%d", test.Name, gas), func(t *testing.T) {
-		_, _, err := RunPrecompiledContract(p, in, gas)
+		_, _, err := RunPrecompiledContract(p, in, gas, mockPrecompileContext)
 		if err.Error() != "out of gas" {
 			t.Errorf("Expected error [out of gas], got [%v]", err)
 		}
@@ -139,7 +139,7 @@ func testPrecompiledFailure(addr string, test precompiledFailureTest, t *testing
 	in := common.Hex2Bytes(test.Input)
 	gas := p.RequiredGas(in)
 	t.Run(test.Name, func(t *testing.T) {
-		_, _, err := RunPrecompiledContract(p, in, gas)
+		_, _, err := RunPrecompiledContract(p, in, gas, mockPrecompileContext)
 		if err.Error() != test.ExpectedError {
 			t.Errorf("Expected error [%v], got [%v]", test.ExpectedError, err)
 		}
@@ -171,7 +171,7 @@ func benchmarkPrecompiled(addr string, test precompiledTest, bench *testing.B) {
 		bench.ResetTimer()
 		for i := 0; i < bench.N; i++ {
 			copy(data, in)
-			res, _, err = RunPrecompiledContract(p, data, reqGas)
+			res, _, err = RunPrecompiledContract(p, data, reqGas, mockPrecompileContext)
 		}
 		bench.StopTimer()
 		elapsed := uint64(time.Since(start))

Tenderly/net-celo

core/vm/contracts.go
@@ -176,8 +176,16 @@ var PrecompiledContractsGranite = map[common.Address]PrecompiledContract{
 	common.BytesToAddress([]byte{0x01, 0x00}): &p256Verify{},
 }
 
+// PrecompiledContractsCel2 contains the default set of pre-compiled Ethereum
+// contracts used in the Cel2 release which don't require the extra
+// celoPrecompileContext, while PrecompiledCeloContractsCel2 contains those
+// that do.
+var PrecompiledContractsCel2 = PrecompiledContractsGranite
+var PrecompiledCeloContractsCel2 = map[common.Address]CeloPrecompiledContract{}
+
 var (
 	PrecompiledAddressesGranite   []common.Address
+	PrecompiledAddressesCel2      []common.Address
 	PrecompiledAddressesFjord     []common.Address
 	PrecompiledAddressesPrague    []common.Address
 	PrecompiledAddressesCancun    []common.Address
@@ -242,8 +250,9 @@ func ActivePrecompiledContracts(rules params.Rules) PrecompiledContracts {
 	return maps.Clone(activePrecompiledContracts(rules))
 }
 
-// ActivePrecompiles returns the precompile addresses enabled with the current configuration.
-func ActivePrecompiles(rules params.Rules) []common.Address {
+// OptimismPrecompiles returns the original Optimism precompiles enabled with
+// the current configuration.
+func OptimismPrecompiles(rules params.Rules) []common.Address {
 	switch {
 	case rules.IsOptimismGranite:
 		return PrecompiledAddressesGranite
@@ -264,12 +273,30 @@ func ActivePrecompiles(rules params.Rules) []common.Address {
 	}
 }
 
+// ActivePrecompiles returns the precompiles enabled with the current configuration.
+func ActivePrecompiles(rules params.Rules) []common.Address {
+	addresses := OptimismPrecompiles(rules)
+
+	if !rules.IsCel2 {
+		return addresses
+	}
+
+	PrecompiledAddressesCel2 = PrecompiledAddressesCel2[:0]
+	PrecompiledAddressesCel2 = append(PrecompiledAddressesCel2, addresses...)
+
+	for k := range PrecompiledCeloContractsCel2 {
+		PrecompiledAddressesCel2 = append(PrecompiledAddressesCel2, k)
+	}
+
+	return PrecompiledAddressesCel2
+}
+
 // RunPrecompiledContract runs and evaluates the output of a precompiled contract.
 // It returns
 // - the returned bytes,
 // - the _remaining_ gas,
 // - any error that occurred
-func RunPrecompiledContract(p PrecompiledContract, input []byte, suppliedGas uint64, logger *tracing.Hooks) (ret []byte, remainingGas uint64, err error) {
+func RunPrecompiledContract(p CeloPrecompiledContract, input []byte, suppliedGas uint64, logger *tracing.Hooks, ctx *celoPrecompileContext) (ret []byte, remainingGas uint64, err error) {
 	gasCost := p.RequiredGas(input)
 	if suppliedGas < gasCost {
 		return nil, 0, ErrOutOfGas
@@ -278,7 +305,7 @@ func RunPrecompiledContract(p PrecompiledContract, input []byte, suppliedGas uin
 		logger.OnGasChange(suppliedGas, suppliedGas-gasCost, tracing.GasChangeCallPrecompiledContract)
 	}
 	suppliedGas -= gasCost
-	output, err := p.Run(input)
+	output, err := p.Run(input, ctx)
 	return output, suppliedGas, err
 }
 

core/vm/contracts_test.go
@@ -46,30 +46,30 @@ type precompiledFailureTest struct {
 
 // allPrecompiles does not map to the actual set of precompiles, as it also contains
 // repriced versions of precompiles at certain slots
-var allPrecompiles = map[common.Address]PrecompiledContract{
-	common.BytesToAddress([]byte{1}):    &ecrecover{},
-	common.BytesToAddress([]byte{2}):    &sha256hash{},
-	common.BytesToAddress([]byte{3}):    &ripemd160hash{},
-	common.BytesToAddress([]byte{4}):    &dataCopy{},
-	common.BytesToAddress([]byte{5}):    &bigModExp{eip2565: false},
-	common.BytesToAddress([]byte{0xf5}): &bigModExp{eip2565: true},
-	common.BytesToAddress([]byte{6}):    &bn256AddIstanbul{},
-	common.BytesToAddress([]byte{7}):    &bn256ScalarMulIstanbul{},
-	common.BytesToAddress([]byte{8}):    &bn256PairingGranite{},
-	common.BytesToAddress([]byte{9}):    &blake2F{},
-	common.BytesToAddress([]byte{0x0a}): &kzgPointEvaluation{},
+var allPrecompiles = map[common.Address]CeloPrecompiledContract{
+	common.BytesToAddress([]byte{1}):    &wrap{&ecrecover{}},
+	common.BytesToAddress([]byte{2}):    &wrap{&sha256hash{}},
+	common.BytesToAddress([]byte{3}):    &wrap{&ripemd160hash{}},
+	common.BytesToAddress([]byte{4}):    &wrap{&dataCopy{}},
+	common.BytesToAddress([]byte{5}):    &wrap{&bigModExp{eip2565: false}},
+	common.BytesToAddress([]byte{0xf5}): &wrap{&bigModExp{eip2565: true}},
+	common.BytesToAddress([]byte{6}):    &wrap{&bn256AddIstanbul{}},
+	common.BytesToAddress([]byte{7}):    &wrap{&bn256ScalarMulIstanbul{}},
+	common.BytesToAddress([]byte{8}):    &wrap{&bn256PairingGranite{}},
+	common.BytesToAddress([]byte{9}):    &wrap{&blake2F{}},
+	common.BytesToAddress([]byte{0x0a}): &wrap{&kzgPointEvaluation{}},
 
-	common.BytesToAddress([]byte{0x01, 0x00}): &p256Verify{},
+	common.BytesToAddress([]byte{0x01, 0x00}): &wrap{&p256Verify{}},
 
-	common.BytesToAddress([]byte{0x0f, 0x0a}): &bls12381G1Add{},
-	common.BytesToAddress([]byte{0x0f, 0x0b}): &bls12381G1Mul{},
-	common.BytesToAddress([]byte{0x0f, 0x0c}): &bls12381G1MultiExp{},
-	common.BytesToAddress([]byte{0x0f, 0x0d}): &bls12381G2Add{},
-	common.BytesToAddress([]byte{0x0f, 0x0e}): &bls12381G2Mul{},
-	common.BytesToAddress([]byte{0x0f, 0x0f}): &bls12381G2MultiExp{},
-	common.BytesToAddress([]byte{0x0f, 0x10}): &bls12381Pairing{},
-	common.BytesToAddress([]byte{0x0f, 0x11}): &bls12381MapG1{},
-	common.BytesToAddress([]byte{0x0f, 0x12}): &bls12381MapG2{},
+	common.BytesToAddress([]byte{0x0f, 0x0a}): &wrap{&bls12381G1Add{}},
+	common.BytesToAddress([]byte{0x0f, 0x0b}): &wrap{&bls12381G1Mul{}},
+	common.BytesToAddress([]byte{0x0f, 0x0c}): &wrap{&bls12381G1MultiExp{}},
+	common.BytesToAddress([]byte{0x0f, 0x0d}): &wrap{&bls12381G2Add{}},
+	common.BytesToAddress([]byte{0x0f, 0x0e}): &wrap{&bls12381G2Mul{}},
+	common.BytesToAddress([]byte{0x0f, 0x0f}): &wrap{&bls12381G2MultiExp{}},
+	common.BytesToAddress([]byte{0x0f, 0x10}): &wrap{&bls12381Pairing{}},
+	common.BytesToAddress([]byte{0x0f, 0x11}): &wrap{&bls12381MapG1{}},
+	common.BytesToAddress([]byte{0x0f, 0x12}): &wrap{&bls12381MapG2{}},
 }
 
 // EIP-152 test vectors
@@ -101,7 +101,7 @@ func testPrecompiled(addr string, test precompiledTest, t *testing.T) {
 	in := common.Hex2Bytes(test.Input)
 	gas := p.RequiredGas(in)
 	t.Run(fmt.Sprintf("%s-Gas=%d", test.Name, gas), func(t *testing.T) {
-		if res, _, err := RunPrecompiledContract(p, in, gas, nil); err != nil {
+		if res, _, err := RunPrecompiledContract(p, in, gas, nil, mockPrecompileContext); err != nil {
 			t.Error(err)
 		} else if common.Bytes2Hex(res) != test.Expected {
 			t.Errorf("Expected %v, got %v", test.Expected, common.Bytes2Hex(res))
@@ -123,7 +123,7 @@ func testPrecompiledOOG(addr string, test precompiledTest, t *testing.T) {
 	gas := p.RequiredGas(in) - 1
 
 	t.Run(fmt.Sprintf("%s-Gas=%d", test.Name, gas), func(t *testing.T) {
-		_, _, err := RunPrecompiledContract(p, in, gas, nil)
+		_, _, err := RunPrecompiledContract(p, in, gas, nil, mockPrecompileContext)
 		if err.Error() != "out of gas" {
 			t.Errorf("Expected error [out of gas], got [%v]", err)
 		}
@@ -140,7 +140,7 @@ func testPrecompiledFailure(addr string, test precompiledFailureTest, t *testing
 	in := common.Hex2Bytes(test.Input)
 	gas := p.RequiredGas(in)
 	t.Run(test.Name, func(t *testing.T) {
-		_, _, err := RunPrecompiledContract(p, in, gas, nil)
+		_, _, err := RunPrecompiledContract(p, in, gas, nil, mockPrecompileContext)
 		if err.Error() != test.ExpectedError {
 			t.Errorf("Expected error [%v], got [%v]", test.ExpectedError, err)
 		}
@@ -172,7 +172,7 @@ func benchmarkPrecompiled(addr string, test precompiledTest, bench *testing.B) {
 		bench.ResetTimer()
 		for i := 0; i < bench.N; i++ {
 			copy(data, in)
-			res, _, err = RunPrecompiledContract(p, data, reqGas, nil)
+			res, _, err = RunPrecompiledContract(p, data, reqGas, nil, mockPrecompileContext)
 		}
 		bench.StopTimer()
 		elapsed := uint64(time.Since(start))