celo-org/op-geth

Allow non-hash based blocknum argument in CeloAPI backend (#138)
* Allow non-hash based blocknum argument in CeloAPI backend

* Remove LRU cache from GetExchangeRates

Lets keep it simple and not do any premature optimization.
If we implement this it might be better to do it on a lower level
anyways.

internal/celoapi/backend.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/exchange"
-	"github.com/ethereum/go-ethereum/common/lru"
 	"github.com/ethereum/go-ethereum/contracts"
 	"github.com/ethereum/go-ethereum/internal/ethapi"
 	"github.com/ethereum/go-ethereum/rpc"
@@ -15,8 +14,7 @@ import (
 
 func NewCeloAPIBackend(b ethapi.Backend) *CeloAPIBackend {
 	return &CeloAPIBackend{
-		Backend:            b,
-		exchangeRatesCache: lru.NewCache[common.Hash, common.ExchangeRates](128),
+		Backend: b,
 	}
 }
 
@@ -24,26 +22,17 @@ func NewCeloAPIBackend(b ethapi.Backend) *CeloAPIBackend {
 // functionality. CeloAPIBackend is mainly passed to the JSON RPC services and provides
 // an easy way to make extra functionality available in the service internal methods without
 // having to change their call signature significantly.
-// CeloAPIBackend keeps a threadsafe LRU cache of block-hash to exchange rates for that block.
-// Cache invalidation is only a problem when an already existing blocks' hash
-// doesn't change, but the rates change. That shouldn't be possible, since changing the rates
-// requires different transaction hashes / state and thus a different block hash.
-// If the previous rates change during a reorg, the previous block hash should also change
-// and with it the new block's hash.
-// Stale branches cache values will get evicted eventually.
 type CeloAPIBackend struct {
 	ethapi.Backend
-
-	exchangeRatesCache *lru.Cache[common.Hash, common.ExchangeRates]
 }
 
-func (b *CeloAPIBackend) getContractCaller(ctx context.Context, atBlock common.Hash) (*contracts.CeloBackend, error) {
+func (b *CeloAPIBackend) getContractCaller(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash) (*contracts.CeloBackend, error) {
 	state, _, err := b.Backend.StateAndHeaderByNumberOrHash(
 		ctx,
-		rpc.BlockNumberOrHashWithHash(atBlock, false),
+		blockNumOrHash,
 	)
 	if err != nil {
-		return nil, fmt.Errorf("retrieve state for block hash %s: %w", atBlock.String(), err)
+		return nil, fmt.Errorf("retrieve state for block hash %s: %w", blockNumOrHash.String(), err)
 	}
 	return &contracts.CeloBackend{
 		ChainConfig: b.Backend.ChainConfig(),
@@ -51,41 +40,36 @@ func (b *CeloAPIBackend) getContractCaller(ctx context.Context, atBlock common.H
 	}, nil
 }
 
-func (b *CeloAPIBackend) GetFeeBalance(ctx context.Context, atBlock common.Hash, account common.Address, feeCurrency *common.Address) (*big.Int, error) {
-	cb, err := b.getContractCaller(ctx, atBlock)
+func (b *CeloAPIBackend) GetFeeBalance(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash, account common.Address, feeCurrency *common.Address) (*big.Int, error) {
+	cb, err := b.getContractCaller(ctx, blockNumOrHash)
 	if err != nil {
 		return nil, err
 	}
 	return contracts.GetFeeBalance(cb, account, feeCurrency), nil
 }
 
-func (b *CeloAPIBackend) GetExchangeRates(ctx context.Context, atBlock common.Hash) (common.ExchangeRates, error) {
-	cachedRates, ok := b.exchangeRatesCache.Get(atBlock)
-	if ok {
-		return cachedRates, nil
-	}
-	cb, err := b.getContractCaller(ctx, atBlock)
+func (b *CeloAPIBackend) GetExchangeRates(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash) (common.ExchangeRates, error) {
+	contractBackend, err := b.getContractCaller(ctx, blockNumOrHash)
 	if err != nil {
 		return nil, err
 	}
-	er, err := contracts.GetExchangeRates(cb)
+	er, err := contracts.GetExchangeRates(contractBackend)
 	if err != nil {
 		return nil, err
 	}
-	b.exchangeRatesCache.Add(atBlock, er)
 	return er, nil
 }
 
-func (b *CeloAPIBackend) ConvertToCurrency(ctx context.Context, atBlock common.Hash, value *big.Int, fromFeeCurrency *common.Address) (*big.Int, error) {
-	er, err := b.GetExchangeRates(ctx, atBlock)
+func (b *CeloAPIBackend) ConvertToCurrency(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash, value *big.Int, fromFeeCurrency *common.Address) (*big.Int, error) {
+	er, err := b.GetExchangeRates(ctx, blockNumOrHash)
 	if err != nil {
 		return nil, err
 	}
 	return exchange.ConvertGoldToCurrency(er, fromFeeCurrency, value)
 }
 
-func (b *CeloAPIBackend) ConvertToGold(ctx context.Context, atBlock common.Hash, value *big.Int, toFeeCurrency *common.Address) (*big.Int, error) {
-	er, err := b.GetExchangeRates(ctx, atBlock)
+func (b *CeloAPIBackend) ConvertToGold(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash, value *big.Int, toFeeCurrency *common.Address) (*big.Int, error) {
+	er, err := b.GetExchangeRates(ctx, blockNumOrHash)
 	if err != nil {
 		return nil, err
 	}

internal/ethapi/api_test.go
@@ -590,9 +590,9 @@ type celoTestBackend struct {
 	*testBackend
 }
 
-func (c *celoTestBackend) GetFeeBalance(ctx context.Context, atBlock common.Hash, account common.Address, feeCurrency *common.Address) (*big.Int, error) {
+func (c *celoTestBackend) GetFeeBalance(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash, account common.Address, feeCurrency *common.Address) (*big.Int, error) {
 	if feeCurrency == nil {
-		header, err := c.HeaderByHash(ctx, atBlock)
+		header, err := c.HeaderByNumberOrHash(ctx, blockNumOrHash)
 		if err != nil {
 			return nil, fmt.Errorf("retrieve header by hash in testBackend: %w", err)
 		}
@@ -607,12 +607,12 @@ func (c *celoTestBackend) GetFeeBalance(ctx context.Context, atBlock common.Hash
 	return nil, errCeloNotImplemented
 }
 
-func (c *celoTestBackend) GetExchangeRates(ctx context.Context, atBlock common.Hash) (common.ExchangeRates, error) {
+func (c *celoTestBackend) GetExchangeRates(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash) (common.ExchangeRates, error) {
 	var er common.ExchangeRates
 	return er, nil
 }
 
-func (c *celoTestBackend) ConvertToCurrency(ctx context.Context, atBlock common.Hash, value *big.Int, feeCurrency *common.Address) (*big.Int, error) {
+func (c *celoTestBackend) ConvertToCurrency(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash, value *big.Int, feeCurrency *common.Address) (*big.Int, error) {
 	if feeCurrency == nil {
 		return value, nil
 	}
@@ -620,7 +620,7 @@ func (c *celoTestBackend) ConvertToCurrency(ctx context.Context, atBlock common.
 	return nil, errCeloNotImplemented
 }
 
-func (c *celoTestBackend) ConvertToGold(ctx context.Context, atBlock common.Hash, value *big.Int, feeCurrency *common.Address) (*big.Int, error) {
+func (c *celoTestBackend) ConvertToGold(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash, value *big.Int, feeCurrency *common.Address) (*big.Int, error) {
 	if feeCurrency == nil {
 		return value, nil
 	}

internal/ethapi/backend.go
@@ -40,10 +40,10 @@ import (
 type CeloBackend interface {
 	Backend
 
-	GetFeeBalance(ctx context.Context, atBlock common.Hash, account common.Address, feeCurrency *common.Address) (*big.Int, error)
-	GetExchangeRates(ctx context.Context, atBlock common.Hash) (common.ExchangeRates, error)
-	ConvertToCurrency(ctx context.Context, atBlock common.Hash, value *big.Int, feeCurrency *common.Address) (*big.Int, error)
-	ConvertToGold(ctx context.Context, atBlock common.Hash, value *big.Int, feeCurrency *common.Address) (*big.Int, error)
+	GetFeeBalance(ctx context.Context, blockNrOrHash rpc.BlockNumberOrHash, account common.Address, feeCurrency *common.Address) (*big.Int, error)
+	GetExchangeRates(ctx context.Context, blockNrOrHash rpc.BlockNumberOrHash) (common.ExchangeRates, error)
+	ConvertToCurrency(ctx context.Context, blockNrOrHash rpc.BlockNumberOrHash, value *big.Int, feeCurrency *common.Address) (*big.Int, error)
+	ConvertToGold(ctx context.Context, blockNrOrHash rpc.BlockNumberOrHash, value *big.Int, feeCurrency *common.Address) (*big.Int, error)
 }
 
 // Backend interface provides the common API services (that are provided by both full and light clients) with access to necessary functions.

internal/ethapi/transaction_args_test.go
@@ -264,23 +264,23 @@ func newCeloBackendMock() *celoBackendMock {
 	}
 }
 
-func (c *celoBackendMock) GetFeeBalance(ctx context.Context, atBlock common.Hash, account common.Address, feeCurrency *common.Address) (*big.Int, error) {
+func (c *celoBackendMock) GetFeeBalance(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash, account common.Address, feeCurrency *common.Address) (*big.Int, error) {
 	// Celo specific backend features are currently not tested
 	return nil, errCeloNotImplemented
 }
 
-func (c *celoBackendMock) GetExchangeRates(ctx context.Context, atBlock common.Hash) (common.ExchangeRates, error) {
+func (c *celoBackendMock) GetExchangeRates(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash) (common.ExchangeRates, error) {
 	var er common.ExchangeRates
 	// Celo specific backend features are currently not tested
 	return er, errCeloNotImplemented
 }
 
-func (c *celoBackendMock) ConvertToCurrency(ctx context.Context, atBlock common.Hash, value *big.Int, fromFeeCurrency *common.Address) (*big.Int, error) {
+func (c *celoBackendMock) ConvertToCurrency(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash, value *big.Int, fromFeeCurrency *common.Address) (*big.Int, error) {
 	// Celo specific backend features are currently not tested
 	return nil, errCeloNotImplemented
 }
 
-func (c *celoBackendMock) ConvertToGold(ctx context.Context, atBlock common.Hash, value *big.Int, toFeeCurrency *common.Address) (*big.Int, error) {
+func (c *celoBackendMock) ConvertToGold(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash, value *big.Int, toFeeCurrency *common.Address) (*big.Int, error) {
 	// Celo specific backend features are currently not tested
 	return nil, errCeloNotImplemented
 }

Tenderly/net-celo

internal/celoapi/backend.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/exchange"
-	"github.com/ethereum/go-ethereum/common/lru"
 	"github.com/ethereum/go-ethereum/contracts"
 	"github.com/ethereum/go-ethereum/internal/ethapi"
 	"github.com/ethereum/go-ethereum/rpc"
@@ -15,8 +14,7 @@ import (
 
 func NewCeloAPIBackend(b ethapi.Backend) *CeloAPIBackend {
 	return &CeloAPIBackend{
-		Backend:            b,
-		exchangeRatesCache: lru.NewCache[common.Hash, common.ExchangeRates](128),
+		Backend: b,
 	}
 }
 
@@ -24,26 +22,17 @@ func NewCeloAPIBackend(b ethapi.Backend) *CeloAPIBackend {
 // functionality. CeloAPIBackend is mainly passed to the JSON RPC services and provides
 // an easy way to make extra functionality available in the service internal methods without
 // having to change their call signature significantly.
-// CeloAPIBackend keeps a threadsafe LRU cache of block-hash to exchange rates for that block.
-// Cache invalidation is only a problem when an already existing blocks' hash
-// doesn't change, but the rates change. That shouldn't be possible, since changing the rates
-// requires different transaction hashes / state and thus a different block hash.
-// If the previous rates change during a reorg, the previous block hash should also change
-// and with it the new block's hash.
-// Stale branches cache values will get evicted eventually.
 type CeloAPIBackend struct {
 	ethapi.Backend
-
-	exchangeRatesCache *lru.Cache[common.Hash, common.ExchangeRates]
 }
 
-func (b *CeloAPIBackend) getContractCaller(ctx context.Context, atBlock common.Hash) (*contracts.CeloBackend, error) {
+func (b *CeloAPIBackend) getContractCaller(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash) (*contracts.CeloBackend, error) {
 	state, _, err := b.Backend.StateAndHeaderByNumberOrHash(
 		ctx,
-		rpc.BlockNumberOrHashWithHash(atBlock, false),
+		blockNumOrHash,
 	)
 	if err != nil {
-		return nil, fmt.Errorf("retrieve state for block hash %s: %w", atBlock.String(), err)
+		return nil, fmt.Errorf("retrieve state for block hash %s: %w", blockNumOrHash.String(), err)
 	}
 	return &contracts.CeloBackend{
 		ChainConfig: b.Backend.ChainConfig(),
@@ -51,41 +40,36 @@ func (b *CeloAPIBackend) getContractCaller(ctx context.Context, atBlock common.H
 	}, nil
 }
 
-func (b *CeloAPIBackend) GetFeeBalance(ctx context.Context, atBlock common.Hash, account common.Address, feeCurrency *common.Address) (*big.Int, error) {
-	cb, err := b.getContractCaller(ctx, atBlock)
+func (b *CeloAPIBackend) GetFeeBalance(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash, account common.Address, feeCurrency *common.Address) (*big.Int, error) {
+	cb, err := b.getContractCaller(ctx, blockNumOrHash)
 	if err != nil {
 		return nil, err
 	}
 	return contracts.GetFeeBalance(cb, account, feeCurrency), nil
 }
 
-func (b *CeloAPIBackend) GetExchangeRates(ctx context.Context, atBlock common.Hash) (common.ExchangeRates, error) {
-	cachedRates, ok := b.exchangeRatesCache.Get(atBlock)
-	if ok {
-		return cachedRates, nil
-	}
-	cb, err := b.getContractCaller(ctx, atBlock)
+func (b *CeloAPIBackend) GetExchangeRates(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash) (common.ExchangeRates, error) {
+	contractBackend, err := b.getContractCaller(ctx, blockNumOrHash)
 	if err != nil {
 		return nil, err
 	}
-	er, err := contracts.GetExchangeRates(cb)
+	exchangeRates, err := contracts.GetExchangeRates(contractBackend)
 	if err != nil {
 		return nil, err
 	}
-	b.exchangeRatesCache.Add(atBlock, er)
-	return er, nil
+	return exchangeRates, nil
 }
 
-func (b *CeloAPIBackend) ConvertToCurrency(ctx context.Context, atBlock common.Hash, value *big.Int, fromFeeCurrency *common.Address) (*big.Int, error) {
-	er, err := b.GetExchangeRates(ctx, atBlock)
+func (b *CeloAPIBackend) ConvertToCurrency(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash, value *big.Int, fromFeeCurrency *common.Address) (*big.Int, error) {
+	er, err := b.GetExchangeRates(ctx, blockNumOrHash)
 	if err != nil {
 		return nil, err
 	}
 	return exchange.ConvertCeloToCurrency(er, fromFeeCurrency, value)
 }
 
-func (b *CeloAPIBackend) ConvertToGold(ctx context.Context, atBlock common.Hash, value *big.Int, toFeeCurrency *common.Address) (*big.Int, error) {
-	er, err := b.GetExchangeRates(ctx, atBlock)
+func (b *CeloAPIBackend) ConvertToCelo(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash, value *big.Int, toFeeCurrency *common.Address) (*big.Int, error) {
+	er, err := b.GetExchangeRates(ctx, blockNumOrHash)
 	if err != nil {
 		return nil, err
 	}

internal/ethapi/api_test.go
@@ -595,9 +595,9 @@ type celoTestBackend struct {
 	*testBackend
 }
 
-func (c *celoTestBackend) GetFeeBalance(ctx context.Context, atBlock common.Hash, account common.Address, feeCurrency *common.Address) (*big.Int, error) {
+func (c *celoTestBackend) GetFeeBalance(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash, account common.Address, feeCurrency *common.Address) (*big.Int, error) {
 	if feeCurrency == nil {
-		header, err := c.HeaderByHash(ctx, atBlock)
+		header, err := c.HeaderByNumberOrHash(ctx, blockNumOrHash)
 		if err != nil {
 			return nil, fmt.Errorf("retrieve header by hash in testBackend: %w", err)
 		}
@@ -612,12 +612,12 @@ func (c *celoTestBackend) GetFeeBalance(ctx context.Context, atBlock common.Hash
 	return nil, errCeloNotImplemented
 }
 
-func (c *celoTestBackend) GetExchangeRates(ctx context.Context, atBlock common.Hash) (common.ExchangeRates, error) {
+func (c *celoTestBackend) GetExchangeRates(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash) (common.ExchangeRates, error) {
 	var er common.ExchangeRates
 	return er, nil
 }
 
-func (c *celoTestBackend) ConvertToCurrency(ctx context.Context, atBlock common.Hash, value *big.Int, feeCurrency *common.Address) (*big.Int, error) {
+func (c *celoTestBackend) ConvertToCurrency(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash, value *big.Int, feeCurrency *common.Address) (*big.Int, error) {
 	if feeCurrency == nil {
 		return value, nil
 	}
@@ -625,7 +625,7 @@ func (c *celoTestBackend) ConvertToCurrency(ctx context.Context, atBlock common.
 	return nil, errCeloNotImplemented
 }
 
-func (c *celoTestBackend) ConvertToGold(ctx context.Context, atBlock common.Hash, value *big.Int, feeCurrency *common.Address) (*big.Int, error) {
+func (c *celoTestBackend) ConvertToCelo(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash, value *big.Int, feeCurrency *common.Address) (*big.Int, error) {
 	if feeCurrency == nil {
 		return value, nil
 	}

internal/ethapi/backend.go
@@ -40,10 +40,10 @@ import (
 type CeloBackend interface {
 	Backend
 
-	GetFeeBalance(ctx context.Context, atBlock common.Hash, account common.Address, feeCurrency *common.Address) (*big.Int, error)
-	GetExchangeRates(ctx context.Context, atBlock common.Hash) (common.ExchangeRates, error)
-	ConvertToCurrency(ctx context.Context, atBlock common.Hash, value *big.Int, feeCurrency *common.Address) (*big.Int, error)
-	ConvertToGold(ctx context.Context, atBlock common.Hash, value *big.Int, feeCurrency *common.Address) (*big.Int, error)
+	GetFeeBalance(ctx context.Context, blockNrOrHash rpc.BlockNumberOrHash, account common.Address, feeCurrency *common.Address) (*big.Int, error)
+	GetExchangeRates(ctx context.Context, blockNrOrHash rpc.BlockNumberOrHash) (common.ExchangeRates, error)
+	ConvertToCurrency(ctx context.Context, blockNrOrHash rpc.BlockNumberOrHash, value *big.Int, feeCurrency *common.Address) (*big.Int, error)
+	ConvertToCelo(ctx context.Context, blockNrOrHash rpc.BlockNumberOrHash, value *big.Int, feeCurrency *common.Address) (*big.Int, error)
 }
 
 // Backend interface provides the common API services (that are provided by both full and light clients) with access to necessary functions.

internal/ethapi/transaction_args_test.go
@@ -266,23 +266,23 @@ func newCeloBackendMock() *celoBackendMock {
 	}
 }
 
-func (c *celoBackendMock) GetFeeBalance(ctx context.Context, atBlock common.Hash, account common.Address, feeCurrency *common.Address) (*big.Int, error) {
+func (c *celoBackendMock) GetFeeBalance(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash, account common.Address, feeCurrency *common.Address) (*big.Int, error) {
 	// Celo specific backend features are currently not tested
 	return nil, errCeloNotImplemented
 }
 
-func (c *celoBackendMock) GetExchangeRates(ctx context.Context, atBlock common.Hash) (common.ExchangeRates, error) {
+func (c *celoBackendMock) GetExchangeRates(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash) (common.ExchangeRates, error) {
 	var er common.ExchangeRates
 	// Celo specific backend features are currently not tested
 	return er, errCeloNotImplemented
 }
 
-func (c *celoBackendMock) ConvertToCurrency(ctx context.Context, atBlock common.Hash, value *big.Int, fromFeeCurrency *common.Address) (*big.Int, error) {
+func (c *celoBackendMock) ConvertToCurrency(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash, value *big.Int, fromFeeCurrency *common.Address) (*big.Int, error) {
 	// Celo specific backend features are currently not tested
 	return nil, errCeloNotImplemented
 }
 
-func (c *celoBackendMock) ConvertToGold(ctx context.Context, atBlock common.Hash, value *big.Int, toFeeCurrency *common.Address) (*big.Int, error) {
+func (c *celoBackendMock) ConvertToCelo(ctx context.Context, blockNumOrHash rpc.BlockNumberOrHash, value *big.Int, toFeeCurrency *common.Address) (*big.Int, error) {
 	// Celo specific backend features are currently not tested
 	return nil, errCeloNotImplemented
 }