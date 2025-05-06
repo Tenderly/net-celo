celo-org/op-geth

Port the block limit per fee currency (#119)
* Port the block limit per fee currency feature

Closes #65

This implements the block-limit per fee-currency feature.
Some parts of this have been directly ported from celo-blockchain
(https://github.com/celo-org/celo-blockchain/commit/dc45bdc00).

* Copy MultiGasPool in miner environment

* Add USDT and USDC to block-limit per currency defaults

* Initialize MultiGasPool with currency whitelist

core/celo_multi_gaspool.go
@@ -1,0 +1,71 @@
+package core
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+)
+
+type FeeCurrency = common.Address
+
+// MultiGasPool tracks the amount of gas available during execution
+// of the transactions in a block per fee currency. The zero value is a pool
+// with zero gas available.
+type MultiGasPool struct {
+	pools       map[FeeCurrency]*GasPool
+	defaultPool *GasPool
+}
+
+type FeeCurrencyLimitMapping = map[FeeCurrency]float64
+
+// NewMultiGasPool creates a multi-fee currency gas pool and a default fallback
+// pool for CELO
+func NewMultiGasPool(
+	blockGasLimit uint64,
+	whitelist []FeeCurrency,
+	defaultLimit float64,
+	limitsMapping FeeCurrencyLimitMapping,
+) *MultiGasPool {
+	pools := make(map[FeeCurrency]*GasPool, len(whitelist))
+
+	for i := range whitelist {
+		currency := whitelist[i]
+		fraction, ok := limitsMapping[currency]
+		if !ok {
+			fraction = defaultLimit
+		}
+
+		pools[currency] = new(GasPool).AddGas(
+			uint64(float64(blockGasLimit) * fraction),
+		)
+	}
+
+	// A special case for CELO which doesn't have a limit
+	celoPool := new(GasPool).AddGas(blockGasLimit)
+
+	return &MultiGasPool{
+		pools:       pools,
+		defaultPool: celoPool,
+	}
+}
+
+// PoolFor returns a configured pool for the given fee currency or the default
+// one otherwise
+func (mgp MultiGasPool) PoolFor(feeCurrency *FeeCurrency) *GasPool {
+	if feeCurrency == nil || mgp.pools[*feeCurrency] == nil {
+		return mgp.defaultPool
+	}
+
+	return mgp.pools[*feeCurrency]
+}
+
+func (mgp MultiGasPool) Copy() *MultiGasPool {
+	pools := make(map[FeeCurrency]*GasPool, len(mgp.pools))
+	for fc, gp := range mgp.pools {
+		gpCpy := *gp
+		pools[fc] = &gpCpy
+	}
+	gpCpy := *mgp.defaultPool
+	return &MultiGasPool{
+		pools:       pools,
+		defaultPool: &gpCpy,
+	}
+}

core/celo_multi_gaspool_test.go
@@ -1,0 +1,138 @@
+package core
+
+import (
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+func TestMultiCurrencyGasPool(t *testing.T) {
+	blockGasLimit := uint64(1_000)
+	subGasAmount := 100
+
+	cUSDToken := common.HexToAddress("0x765DE816845861e75A25fCA122bb6898B8B1282a")
+	cEURToken := common.HexToAddress("0xD8763CBa276a3738E6DE85b4b3bF5FDed6D6cA73")
+
+	testCases := []struct {
+		name                string
+		feeCurrency         *FeeCurrency
+		whitelist           []FeeCurrency
+		defaultLimit        float64
+		limits              FeeCurrencyLimitMapping
+		defaultPoolExpected bool
+		expectedValue       uint64
+	}{
+		{
+			name:                "Empty whitelist, empty mapping, CELO uses default pool",
+			feeCurrency:         nil,
+			whitelist:           []FeeCurrency{},
+			defaultLimit:        0.9,
+			limits:              map[FeeCurrency]float64{},
+			defaultPoolExpected: true,
+			expectedValue:       900, // blockGasLimit - subGasAmount
+		},
+		{
+			name:        "Non-empty whitelist, non-empty mapping, CELO uses default pool",
+			feeCurrency: nil,
+			whitelist: []FeeCurrency{
+				cUSDToken,
+			},
+			defaultLimit: 0.9,
+			limits: map[FeeCurrency]float64{
+				cUSDToken: 0.5,
+			},
+			defaultPoolExpected: true,
+			expectedValue:       900, // blockGasLimit - subGasAmount
+		},
+		{
+			name:                "Empty whitelist, empty mapping, non-whitelisted currency fallbacks to the default pool",
+			feeCurrency:         &cUSDToken,
+			whitelist:           []FeeCurrency{},
+			defaultLimit:        0.9,
+			limits:              map[FeeCurrency]float64{},
+			defaultPoolExpected: true,
+			expectedValue:       900, // blockGasLimit - subGasAmount
+		},
+		{
+			name:        "Non-empty whitelist, non-empty mapping, non-whitelisted currency uses default pool",
+			feeCurrency: &cEURToken,
+			whitelist: []FeeCurrency{
+				cUSDToken,
+			},
+			defaultLimit: 0.9,
+			limits: map[FeeCurrency]float64{
+				cUSDToken: 0.5,
+			},
+			defaultPoolExpected: true,
+			expectedValue:       900, // blockGasLimit - subGasAmount
+		},
+		{
+			name:        "Non-empty whitelist, empty mapping, whitelisted currency uses default limit",
+			feeCurrency: &cUSDToken,
+			whitelist: []FeeCurrency{
+				cUSDToken,
+			},
+			defaultLimit:        0.9,
+			limits:              map[FeeCurrency]float64{},
+			defaultPoolExpected: false,
+			expectedValue:       800, // blockGasLimit * defaultLimit - subGasAmount
+		},
+		{
+			name:        "Non-empty whitelist, non-empty mapping, configured whitelisted currency uses configured limits",
+			feeCurrency: &cUSDToken,
+			whitelist: []FeeCurrency{
+				cUSDToken,
+			},
+			defaultLimit: 0.9,
+			limits: map[FeeCurrency]float64{
+				cUSDToken: 0.5,
+			},
+			defaultPoolExpected: false,
+			expectedValue:       400, // blockGasLimit * 0.5 - subGasAmount
+		},
+		{
+			name:        "Non-empty whitelist, non-empty mapping, unconfigured whitelisted currency uses default limit",
+			feeCurrency: &cEURToken,
+			whitelist: []FeeCurrency{
+				cUSDToken,
+				cEURToken,
+			},
+			defaultLimit: 0.9,
+			limits: map[FeeCurrency]float64{
+				cUSDToken: 0.5,
+			},
+			defaultPoolExpected: false,
+			expectedValue:       800, // blockGasLimit * 0.5 - subGasAmount
+		},
+	}
+
+	for _, c := range testCases {
+		t.Run(c.name, func(t *testing.T) {
+			mgp := NewMultiGasPool(
+				blockGasLimit,
+				c.whitelist,
+				c.defaultLimit,
+				c.limits,
+			)
+
+			pool := mgp.PoolFor(c.feeCurrency)
+			pool.SubGas(uint64(subGasAmount))
+
+			if c.defaultPoolExpected {
+				result := mgp.PoolFor(nil).Gas()
+				if result != c.expectedValue {
+					t.Error("Default pool expected", c.expectedValue, "got", result)
+				}
+			} else {
+				result := mgp.PoolFor(c.feeCurrency).Gas()
+
+				if result != c.expectedValue {
+					t.Error(
+						"Expected pool", c.feeCurrency, "value", c.expectedValue,
+						"got", result,
+					)
+				}
+			}
+		})
+	}
+}

Tenderly/net-celo

core/celo_multi_gaspool.go
@@ -1,0 +1,71 @@
+package core
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+)
+
+type FeeCurrency = common.Address
+
+// MultiGasPool tracks the amount of gas available during execution
+// of the transactions in a block per fee currency. The zero value is a pool
+// with zero gas available.
+type MultiGasPool struct {
+	pools       map[FeeCurrency]*GasPool
+	defaultPool *GasPool
+}
+
+type FeeCurrencyLimitMapping = map[FeeCurrency]float64
+
+// NewMultiGasPool creates a multi-fee currency gas pool and a default fallback
+// pool for CELO
+func NewMultiGasPool(
+	blockGasLimit uint64,
+	allowlist []FeeCurrency,
+	defaultLimit float64,
+	limitsMapping FeeCurrencyLimitMapping,
+) *MultiGasPool {
+	pools := make(map[FeeCurrency]*GasPool, len(allowlist))
+
+	for i := range allowlist {
+		currency := allowlist[i]
+		fraction, ok := limitsMapping[currency]
+		if !ok {
+			fraction = defaultLimit
+		}
+
+		pools[currency] = new(GasPool).AddGas(
+			uint64(float64(blockGasLimit) * fraction),
+		)
+	}
+
+	// A special case for CELO which doesn't have a limit
+	celoPool := new(GasPool).AddGas(blockGasLimit)
+
+	return &MultiGasPool{
+		pools:       pools,
+		defaultPool: celoPool,
+	}
+}
+
+// PoolFor returns a configured pool for the given fee currency or the default
+// one otherwise
+func (mgp MultiGasPool) PoolFor(feeCurrency *FeeCurrency) *GasPool {
+	if feeCurrency == nil || mgp.pools[*feeCurrency] == nil {
+		return mgp.defaultPool
+	}
+
+	return mgp.pools[*feeCurrency]
+}
+
+func (mgp MultiGasPool) Copy() *MultiGasPool {
+	pools := make(map[FeeCurrency]*GasPool, len(mgp.pools))
+	for fc, gp := range mgp.pools {
+		gpCpy := *gp
+		pools[fc] = &gpCpy
+	}
+	gpCpy := *mgp.defaultPool
+	return &MultiGasPool{
+		pools:       pools,
+		defaultPool: &gpCpy,
+	}
+}

core/celo_multi_gaspool_test.go
@@ -1,0 +1,138 @@
+package core
+
+import (
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+func TestMultiCurrencyGasPool(t *testing.T) {
+	blockGasLimit := uint64(1_000)
+	subGasAmount := 100
+
+	cUSDToken := common.HexToAddress("0x765DE816845861e75A25fCA122bb6898B8B1282a")
+	cEURToken := common.HexToAddress("0xD8763CBa276a3738E6DE85b4b3bF5FDed6D6cA73")
+
+	testCases := []struct {
+		name                string
+		feeCurrency         *FeeCurrency
+		allowlist           []FeeCurrency
+		defaultLimit        float64
+		limits              FeeCurrencyLimitMapping
+		defaultPoolExpected bool
+		expectedValue       uint64
+	}{
+		{
+			name:                "Empty allowlist, empty mapping, CELO uses default pool",
+			feeCurrency:         nil,
+			allowlist:           []FeeCurrency{},
+			defaultLimit:        0.9,
+			limits:              map[FeeCurrency]float64{},
+			defaultPoolExpected: true,
+			expectedValue:       900, // blockGasLimit - subGasAmount
+		},
+		{
+			name:        "Non-empty allowlist, non-empty mapping, CELO uses default pool",
+			feeCurrency: nil,
+			allowlist: []FeeCurrency{
+				cUSDToken,
+			},
+			defaultLimit: 0.9,
+			limits: map[FeeCurrency]float64{
+				cUSDToken: 0.5,
+			},
+			defaultPoolExpected: true,
+			expectedValue:       900, // blockGasLimit - subGasAmount
+		},
+		{
+			name:                "Empty allowlist, empty mapping, non-registered currency fallbacks to the default pool",
+			feeCurrency:         &cUSDToken,
+			allowlist:           []FeeCurrency{},
+			defaultLimit:        0.9,
+			limits:              map[FeeCurrency]float64{},
+			defaultPoolExpected: true,
+			expectedValue:       900, // blockGasLimit - subGasAmount
+		},
+		{
+			name:        "Non-empty allowlist, non-empty mapping, non-registered currency uses default pool",
+			feeCurrency: &cEURToken,
+			allowlist: []FeeCurrency{
+				cUSDToken,
+			},
+			defaultLimit: 0.9,
+			limits: map[FeeCurrency]float64{
+				cUSDToken: 0.5,
+			},
+			defaultPoolExpected: true,
+			expectedValue:       900, // blockGasLimit - subGasAmount
+		},
+		{
+			name:        "Non-empty allowlist, empty mapping, registered currency uses default limit",
+			feeCurrency: &cUSDToken,
+			allowlist: []FeeCurrency{
+				cUSDToken,
+			},
+			defaultLimit:        0.9,
+			limits:              map[FeeCurrency]float64{},
+			defaultPoolExpected: false,
+			expectedValue:       800, // blockGasLimit * defaultLimit - subGasAmount
+		},
+		{
+			name:        "Non-empty allowlist, non-empty mapping, configured registered currency uses configured limits",
+			feeCurrency: &cUSDToken,
+			allowlist: []FeeCurrency{
+				cUSDToken,
+			},
+			defaultLimit: 0.9,
+			limits: map[FeeCurrency]float64{
+				cUSDToken: 0.5,
+			},
+			defaultPoolExpected: false,
+			expectedValue:       400, // blockGasLimit * 0.5 - subGasAmount
+		},
+		{
+			name:        "Non-empty allowlist, non-empty mapping, unconfigured registered currency uses default limit",
+			feeCurrency: &cEURToken,
+			allowlist: []FeeCurrency{
+				cUSDToken,
+				cEURToken,
+			},
+			defaultLimit: 0.9,
+			limits: map[FeeCurrency]float64{
+				cUSDToken: 0.5,
+			},
+			defaultPoolExpected: false,
+			expectedValue:       800, // blockGasLimit * 0.5 - subGasAmount
+		},
+	}
+
+	for _, c := range testCases {
+		t.Run(c.name, func(t *testing.T) {
+			mgp := NewMultiGasPool(
+				blockGasLimit,
+				c.allowlist,
+				c.defaultLimit,
+				c.limits,
+			)
+
+			pool := mgp.PoolFor(c.feeCurrency)
+			pool.SubGas(uint64(subGasAmount))
+
+			if c.defaultPoolExpected {
+				result := mgp.PoolFor(nil).Gas()
+				if result != c.expectedValue {
+					t.Error("Default pool expected", c.expectedValue, "got", result)
+				}
+			} else {
+				result := mgp.PoolFor(c.feeCurrency).Gas()
+
+				if result != c.expectedValue {
+					t.Error(
+						"Expected pool", c.feeCurrency, "value", c.expectedValue,
+						"got", result,
+					)
+				}
+			}
+		})
+	}
+}